e83de1c7f1ba6058aee2cd646fa97f7283b31971
19041126 <19041126@LAPTOP-LQ5OJT8J>
2020-07-22 16:28:47 +0800

    first commit

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -247,7 +247,7 @@ public class ResourceCentre {
 			System.out.println("Camcorder " + tag + " returned");
 		}
 	}
-//tbc junwei test
+//tbc junwei test2
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList,String tag){
 		boolean isReturned = false;
 		// write your code here

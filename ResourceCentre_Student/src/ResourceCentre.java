2a2f2d3eca37121b459a4c2935172a0260e69472
19026820 <19026820@myrp.edu.sg>
2020-07-22 20:46:36 +0800

    complete add method

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -181,6 +181,11 @@ public class ResourceCentre {
 	public static Chromebook inputChromebook() {	
 		Chromebook cb =null;
 		// write your code here
+		String tag = Helper.readString("Enter asset tag > ");
+        String description = Helper.readString("enter description > ");
+        String os = Helper.readString("Enter Operating System > ");
+        cb = new Chromebook(tag,description,os);
+   
 		return cb;
 		
 	}	
